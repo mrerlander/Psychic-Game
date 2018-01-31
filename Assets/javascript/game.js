ad33be6688538d17bd207d844ec5e4e1a78f1e22
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-01-30 21:40:07 -0600

    pre-game html and psuedo code

---
+++ Assets/javascript/game.js
@@ -0,0 +1,21 @@
+//variables for computer guess, attempts, wins, and losses
+
+//add function for .onkeyup event
+
+//variable to store keypress
+
+//variable to choose random number from computer array
+
+//if statement for user input (letters only)
+
+//nested if statement for correct guess (wins++ reset attempts)
+
+//else if incorrect guess (show letters attempted attempts--)
+
+//create new html variable
+
+//print new html to document
+
+//else press a valid key
+
+
