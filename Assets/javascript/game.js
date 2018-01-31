df35dce680438f0a58a8e32bd4816029c4e18816
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-01-31 15:37:21 -0600

    removed unused css and images folders/files final-ish versions of html and js

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,21 +1,58 @@
-//variables for computer guess, attempts, wins, and losses
-
-//add function for .onkeyup event
-
-//variable to store keypress
+//variables for computer guess, guess history, attempts, wins, and losses
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var attempts = 10;
+var wins = 0;
+var losses = 0;
 
 //variable to choose random number from computer array
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-//if statement for user input (letters only)
+//add function for .onkeyup event
+document.onkeyup = function (event) {
 
-//nested if statement for correct guess (wins++ reset attempts)
+    //variable to store keypress
+    var userGuess = event.key;
+    var nonLetter = document.getElementById("non-letters");
 
-//else if incorrect guess (show letters attempted attempts--)
+    //if statement for user input (letters only)
+    if (computerChoices.includes(userGuess)) {
 
-//create new html variable
+        nonLetter.innerHTML = "";
 
-//print new html to document
+        document.getElementById("guesses").innerHTML += userGuess;
 
-//else press a valid key
+        //if statement for correct guess
+        if (userGuess === computerGuess) {
+            wins++;
+            attempts = 10;
+            document.getElementById("guesses").innerHTML = "";
+            document.getElementById("computer-letter").innerHTML = "<p> Yes! The letter was: " + computerGuess + "</p>";
+            document.getElementById("wins").innerHTML = wins;
+            document.getElementById("attempts").innerHTML = "10";
+            computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        }
 
+        //else if incorrect guess (show letters attempted attempts--)
+        else if (attempts > 1) {
+            attempts--;
+            document.getElementById("attempts").innerHTML = attempts;
+        }
 
+        //restart
+        else {
+            losses++;
+            attempts = 10;
+            document.getElementById("guesses").innerHTML = "";
+            document.getElementById("losses").innerHTML = losses;
+            document.getElementById("attempts").innerHTML = "10";
+            document.getElementById("computer-letter").innerHTML = "<p> The letter was: " + computerGuess + "</p>";
+            console.log(computerGuess);
+        }
+    }
+
+    //else press a valid key
+    else {
+        nonLetter.innerHTML = "Letters Only, Please";
+    }
+
+};
